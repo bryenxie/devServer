e94da28d5125ea0246427a89d6e7db6528ca993b
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-04 01:47:49 -0400

    add uri deconder

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express')
 var app = express()
+var base64Img = require('base64-img');
 
 app.get('/', function (req, res) {
+  let uri = req.body  
+  base64Img.img(uri,'', '1',function(err, filepath) 
+  
+  {console.log('success')})
   res.send('Hello World')
 })
 
