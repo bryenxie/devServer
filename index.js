68d9839ff69622d110fbe368e03ae2a258e4db7a
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-04 01:46:02 -0500

    add vision stuff

--- index.js
+++ index.js
@@ -7,8 +7,37 @@ app.use(bodyParser.json({limit: '10mb'}));
 
 app.post('/', function (req, res) {
   let uri = req.body.uri  
-  base64Img.img(uri,'', '1',function(err, filepath) 
-  {console.log('success')})
+  base64Img.img(uri,'', '1', function(err, filepath) {
+    console.log('success')
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    // const fileName = 'Local image file, e.g. /path/to/image.png';
+
+    client
+      .faceDetection(fileName)
+      .then(results => {
+        const faces = results[0].faceAnnotations;
+
+        console.log('Faces:');
+        faces.forEach((face, i) => {
+          console.log(`  Face #${i + 1}:`);
+          console.log(`    Joy: ${face.joyLikelihood}`);
+          console.log(`    Anger: ${face.angerLikelihood}`);
+          console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+          console.log(`    Surprise: ${face.surpriseLikelihood}`);
+        });
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
+  })
   res.send('Hello World')
 })
 
