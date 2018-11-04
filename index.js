93e038a7a63e66a1e9bd7e433477b3426579f20d
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-04 01:42:42 -0500

    fixes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express()
 var base64Img = require('base64-img');
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  let uri = req.body  
+app.use(bodyParser.json({limit: '10mb'}));
+
+app.post('/', function (req, res) {
+  let uri = req.body.uri  
   base64Img.img(uri,'', '1',function(err, filepath) 
-  
   {console.log('success')})
   res.send('Hello World')
 })
